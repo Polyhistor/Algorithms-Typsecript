9c454e0067cce541a8b0c4bcbc8a73232fabb7aa
Pouya Ataei <pouya-ataei@idexx.com>
2024-05-28 20:33:29 +1200

    started rewritingh the quick sort

--- quickSortPy.ts
+++ quickSortPy.ts
@@ -1,40 +1,11 @@
-const partitioner = (
+const sorter = (
   array: number[],
   lowestIndex: number,
   highestIndex: number
-): number => {
-  // we start from negative one because we should always be behind the actual iteration index
-  let pivotIndex = -1;
-  const pivot = array[array.length - 1];
+) => {};
 
-  for (let i = 0; i < highestIndex; i++) {
-    if (array[i] <= pivot) {
-      // let's move the index
-      pivotIndex++;
+const quickSort = (array: number[]) => {
+  const sortedArray = sorter(array, 0, array.length - 1);
 
-      if (array[i] !== array[pivotIndex]) {
-        const temp = array[i];
-        array[i] = array[pivotIndex];
-        array[i] = temp;
-      }
-    }
-  }
-
-  pivotIndex++;
-  array[pivotIndex] = array[highestIndex];
-};
-
-const sorter = (array: number[], lowestIndex: number, highestIndex: number) => {
-  // corner case
-  if (lowestIndex >= highestIndex) {
-    return;
-  }
-
-  const pivotIndex = partitioner(array, lowestIndex, highestIndex);
-
-  sorter(array, pivotIndex - 1, pivotIndex + 1);
-};
-
-const quicksort = (array: number[]) => {
-  const pivotIndex = sorter(array, 0, array.length - 1);
+  return sortedArray;
 };
