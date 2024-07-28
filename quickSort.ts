dfb8897c4494c23d3e3c042a9b488128058fc42d
Pouya <pouya.ataei.7@gmail.com>
2024-07-28 18:28:41 +1200

    implemented most of the algo

--- quickSort.ts
+++ quickSort.ts
@@ -1,6 +1,33 @@
-function qs(arr:number[], lo:number, hi:number): void{}
+function qs(arr:number[], lo:number, hi:number): void{
+  if (lo => hi) {
+  return; 
+  }
 
-function partition(arr:number, lo:number, hi:number): number {}
+  const pivotIdx = partition(arr, lo, hi)
+
+  qs(arr,lo, pivotIdx -1)
+}
+
+function partition(arr:number, lo:number, hi:number): number {
+  const pivot = arr[hi]
+
+  let idx = lo - 1 
+
+  for(let i = lo; i < hi; i++) {
+    if(arr[i] <= pivot){
+      idx++ 
+      const tmp = arr[i]
+      arr[i] = arr[idx]
+      arr[idx] = tmp 
+    }
+  }
+
+  idx++
+  arr[hi] = arr[idx]
+  arr[idx] = pivot 
+
+  return idx;
+}
 
 export default function quicksort (arr:number[]): void {
  qs(arr,0, arr.length - 1)
